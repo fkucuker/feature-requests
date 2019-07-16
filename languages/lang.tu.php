8c576c362f9f5fd987c44c42a42ab04a0e8f674d
fkucuker <52867186+fkucuker@users.noreply.github.com>
2019-07-17 00:01:42 +0300

    Create lang.tu.php

---
+++ languages/lang.tu.php
@@ -0,0 +1,56 @@
+<?php
+/* 
+------------------
+Language: English
+------------------
+*/
+$lang = array();
+// Haftanın Günleri
+$lang['monday'] = 'Pazartesi';
+$lang['monday_abbr'] = 'Pzt';
+$lang['tuesday'] = 'Salı';
+$lang['tuesday_abbr'] = 'Sa';
+$lang['wednesday'] = 'Çarşamba';
+$lang['wednesday_abbr'] = 'Çar';
+$lang['thursday'] = 'Perşembe';
+$lang['thursday_abbr'] = 'Per';
+$lang['friday'] = 'Cuma';
+$lang['friday_abbr'] = 'Cum';
+$lang['saturday'] = 'Cumartesi';
+$lang['saturday_abbr'] = 'Cmt';
+$lang['sunday'] = 'Pazar';
+$lang['sunday_abbr'] = 'Pzr';
+// Yılın ayları
+$lang['january'] = 'Ocak';
+$lang['january_abbr'] = 'Ock';
+$lang['february'] = 'Şubat';
+$lang['february_abbr'] = 'Şub';
+$lang['march'] = 'Mart';
+$lang['march_abbr'] = 'Mar';
+$lang['april'] = 'Nisan';
+$lang['april_abbr'] = 'Nis';
+$lang['may'] = 'Mayıs';
+$lang['may_abbr'] = 'May';
+$lang['june'] = 'Haziran';
+$lang['june_abbr'] = 'Haz';
+$lang['july'] = 'Temmuz';
+$lang['july_abbr'] = 'Tem';
+$lang['august'] = 'Ağustos';
+$lang['august_abbr'] = 'Ağts';
+$lang['september'] = 'Eylül';
+$lang['september_abbr'] = 'Eyl';
+$lang['october'] = 'Ekim';
+$lang['october_abbr'] = 'Ekm';
+$lang['november'] = 'Kasım';
+$lang['november_abbr'] = 'Ksm';
+$lang['december'] = 'Aralık';
+$lang['december_abbr'] = 'Arlk';
+// Other terms used
+$lang['today'] = 'Bugün';
+$lang['tomorrow'] = 'Yarın';
+$lang['all_day'] = 'Tüm gün';
+$lang['calendar'] = 'Takvim';
+$lang['weather'] = 'Hava Durumu';
+$lang['todo'] = 'Yapılacaklar';
+$lang['loading'] = 'Yükleniyor';
+?>
